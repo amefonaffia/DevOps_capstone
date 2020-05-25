b62541b0967cd51188e667e049a2aee04e83f599
Abasi-amefon Affia <amefonaffia@gmail.com>
2020-05-25 03:58:04 +0300

    update

--- cloudformationcluster.sh
+++ cloudformationcluster.sh
@@ -2,6 +2,7 @@
 sudo apt update && sudo apt -y install docker.io && sudo systemctl start docker && sudo systemctl enable docker && sudo apt update && sudo apt -y install tidy && sudo chmod 666 /var/run/docker.sock
 apt-get update && apt-get install unzip awscli -y
 apt-get install apache2 -y
+aws configure --region us-east-2
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl &&
 chmod +x ./kubectl && mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
